471c117320b808060f7a93202927bcd34cb43051
Sean Michael Shea <seanshea1337@gmail.com>
2020-12-11 21:09:24 -0500

    test cors

--- assets/javascript/love-app.js
+++ assets/javascript/love-app.js
@@ -46,6 +46,7 @@ function poemLines(loveName) {
 		"url": poemURL,
 		"method": "GET",
 		"headers": {
+			"Access-Control-Allow-Origin": "*",
 			"x-rapidapi-key": "2ab08fa6e5msh6a35a71dc08653cp1d8de2jsn9999fa7a0623",
 			"x-rapidapi-host": "thundercomb-poetry-db-v1.p.rapidapi.com"
 		}
